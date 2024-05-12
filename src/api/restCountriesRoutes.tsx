e6eb12b109643d73b57c111f55b41220d3bd2c88
John Jezon Barlisan Ajias <johnjezonajias@gmail.com>
2024-05-12 15:11:30 +0800

    Add: Functionalities for fetching
    Add functions to retrieve countries by region, subregion, and language.

--- src/api/restCountriesRoutes.tsx
+++ src/api/restCountriesRoutes.tsx
@@ -2,4 +2,12 @@ const baseUrl = 'https://restcountries.com/v3.1';
 
 export const getAllCountries = () => baseUrl + '/all';
 
+export const getCountriesByName = (name: string) => baseUrl + `/name/${name}?fullText=true`;
+
 export const getCountriesByCode = (code: string) => baseUrl + `/alpha/${code}`;
+
+export const getCountriesByRegion = (region: string) => baseUrl + `/region/${region}`;
+
+export const getCountriesBySubRegion = (subregion: string) => baseUrl + `/subregion/${subregion}`;
+
+export const getCountriesByLanguage = (language: string) => baseUrl + `/lang/${language}`;
