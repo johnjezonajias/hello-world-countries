dd9717bdd82f659c1f7448148d6be2817b08d5ff
John Jezon Barlisan Ajias <johnjezonajias@gmail.com>
2024-05-12 15:07:15 +0800

    Update: Improve navigation

--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -2,17 +2,15 @@ import Link from 'next/link';
 
 export const NavigationMenu = () => {
     return (
-        <nav className="p-4">
-            <div className="max-w-7xl mx-auto">
-                <div className="flex justify-between">
-                    <div className="flex space-x-10">
-                        <Link className="text-white hover:text-gray-300" href="/">
-                            Home
-                        </Link>
-                        <Link className="text-white hover:text-gray-300" href="/countries">
-                            Countries
-                        </Link>
-                    </div>
+        <nav className="w-auto">
+            <div className="flex justify-between">
+                <div className="text-sm flex space-x-10">
+                    <Link className="text-white hover:text-gray-300" href="/">
+                        Home
+                    </Link>
+                    <Link className="text-white hover:text-gray-300" href="/countries">
+                        Countries
+                    </Link>
                 </div>
             </div>
         </nav>
